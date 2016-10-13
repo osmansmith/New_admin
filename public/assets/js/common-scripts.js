798ab13ab84948e3c0aedf4e15a175f7108439b7
Bruno <info@proyectarse.com>
2016-10-13 11:58:45 -0300

    cambios comun

--- public/assets/js/common-scripts.js
+++ public/assets/js/common-scripts.js
@@ -105,15 +105,15 @@ var Script = function () {
 
 // custom bar chart
 
-    if ($(".custom-bar-chart")) {
-        $(".bar").each(function () {
-            var i = $(this).find(".value").html();
-            $(this).find(".value").html("");
-            $(this).find(".value").animate({
-                height: i
-            }, 2000)
-        })
-    }
+    // if ($(".custom-bar-chart")) {
+    //     $(".bar").each(function () {
+    //         var i = $(this).find(".value").html();
+    //         $(this).find(".value").html("");
+    //         $(this).find(".value").animate({
+    //             height: i
+    //         }, 2000)
+    //     })
+    // }
 
 
 }();
--- views/add/js_comun.php
+++ views/add/js_comun.php
@@ -7,80 +7,14 @@
     <script class="include" type="text/javascript" src="<?php echo URL?>public/assets/js/jquery.dcjqaccordion.2.7.js"></script>
     <script src="<?php echo URL?>public/assets/js/jquery.scrollTo.min.js"></script>
     <script src="<?php echo URL?>public/assets/js/jquery.nicescroll.js" type="text/javascript"></script>
-    <script src="<?php echo URL?>public/assets/js/jquery.sparkline.js"></script>
 
 
     <!--common script for all pages-->
     <script src="<?php echo URL?>public/assets/js/common-scripts.js"></script>
     
-    <script type="text/javascript" src="<?php echo URL?>public/assets/js/gritter/js/jquery.gritter.js"></script>
-    <script type="text/javascript" src="<?php echo URL?>public/assets/js/gritter-conf.js"></script>
-
-    <!--script for this page-->
-    <script src="<?php echo URL?>public/assets/js/sparkline-chart.js"></script>    
-	<script src="<?php echo URL?>public/assets/js/zabuto_calendar.js"></script>	
-	
-	<script type="text/javascript">
-      
-        $(document).ready(function () {
-        var unique_id = $.gritter.add({
-            // (string | mandatory) the heading of the notification
-            title: 'Welcome to Dashgum!',
-            // (string | mandatory) the text inside the notification
-            text: 'gdfgdf gfdg fdgdfg f gdfgdfgdg.',
-            // (string | optional) the image to display on the left
-            image: '<?php echo URL?>public/assets/img/ui-sam.jpg',
-            // (bool | optional) if you want it to fade out on its own or just sit there
-            sticky: true,
-            // (int | optional) the time you want it to be alive for before fading out
-            time: '',
-            // (string | optional) the class name you want to apply to that specific message
-            class_name: 'my-sticky-class'
-        });
-
-        return false;
-        });
-        
-	</script>
-	
-	<script type="application/javascript">
-        
-       
-        
-        $(document).ready(function () {
-            $("#date-popover").popover({html: true, trigger: "manual"});
-            $("#date-popover").hide();
-            $("#date-popover").click(function (e) {
-                $(this).hide();
-            });
-        
-            $("#my-calendar").zabuto_calendar({
-                action: function () {
-                    return myDateFunction(this.id, false);
-                },
-                action_nav: function () {
-                    return myNavFunction(this.id);
-                },
-                ajax: {
-                    url: "show_data.php?action=1",
-                    modal: true
-                },
-                legend: [
-                    {type: "text", label: "Special event", badge: "00"},
-                    {type: "block", label: "Regular event", }
-                ]
-            });
-        });
-        
-        
-        function myNavFunction(id) {
-            $("#date-popover").hide();
-            var nav = $("#" + id).data("navigation");
-            var to = $("#" + id).data("to");
-            console.log('nav ' + nav + ' to: ' + to.month + '/' + to.year);
-        }
-         
-    </script>
+    <!-- Librerias para las notificaciones flotantes
+    <script type="text/javascript" src="<?php //echo URL?>public/assets/js/gritter/js/jquery.gritter.js"></script>
+    <script type="text/javascript" src="<?php //echo URL?>public/assets/js/gritter-conf.js"></script> -->
   
 
   </body>
